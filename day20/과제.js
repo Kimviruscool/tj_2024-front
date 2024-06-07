98f8d50adf1faf83853909988bbed3b22051dcaf
Kimviruscool <sinsa9122@naver.com>
2024-06-07 09:33:12 +0900

    day21

---
+++ day20/과제.js
@@ -0,0 +1,27 @@
+let boardlist = [];
+
+function add() { console.log('add()');
+     location.href="wirte.html";
+}
+printboard();
+function printboard() {
+    
+    boardlist = JSON.parse(localStorage.getItem('boardlist'));
+    if(boardlist = null){boardlist = [];}
+
+    //어디에
+    let boardtbody = document.querySelector('#boardtbody');
+    //무엇을
+    let html = ``;
+    for(let i = 0 ; i < boardlist.length ; i++){
+        html += `<tr>
+                 <th> ${boardlist[i].no} </th>
+                 <th> ${boardlist[i].제목} </th>
+                 <th> ${boardlist[i].writer} </th>
+                 <th> ${boardlist[i].date} </th>
+                 <th> ${boardlist[i].view} </th>
+                 <tr>`
+    }
+    //출력
+    boardtbody.innerHTML = html;
+}
