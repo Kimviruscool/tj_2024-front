b0705c86cd66d652c1f594b0ec1a0b5feb6d0be9
Kimviruscool <sinsa9122@naver.com>
2024-06-07 09:02:04 +0900

    day20

--- day19/board.js
+++ day19/board.js
@@ -1,29 +1,31 @@
-let 게시판 = [
-    {번호 : 1, 제목 : '공부', 작성자 : '11111', 작성일 : '2024-06-03', 조회수 : '1'}
-]
+/*
 
-let memberList = [];
-화면출력();
-function 화면출력() {
-    //어디에 
-    let 글쓰기출력 = document.querySelector('#글쓰기출력');
-    //무엇을
-    let html = ``;
+  전체 글 출력 페이지 
+    - 글 등록시 모든 게시물이 localStorage 저장된 상태
+*/
 
-    for(let i = 0 ; i < 게시판.length; i++){
-        html += `<tr>
-                    <td>${게시판[i].번호}</td>
-                    <td>${게시판[i].제목}</td>
-                    <td>${게시판[i].작성자}</td>
-                    <td>${게시판[i].작성일}</td>
-                    <td>${게시판[i].조회수}</td>
-                </tr>`
-    }
+let boardList = [];
 
-    //출력
-    글쓰기출력.innerHTML = html;
-}
-
-function tpclick() {
-    location.href="write.html"
+// 1. 출력함수 : js가 열렸을때
+boardPrint();
+function boardPrint(){
+  // * 
+  boardList = JSON.parse( localStorage.getItem( 'boardList' )) ;
+  if( boardList == null ){ boardList = [ ]; }
+  
+  // 1. 어디에
+  let boardTbody = document.querySelector('#boardTbody'); 
+  // 2. 무엇을
+  let html = '';
+  for( let i = 0 ; i < boardList.length ; i++  ){
+    html += `<tr> 
+              <th> ${ boardList[i].no }  </th> 
+              <th> <a href="view.html?no=${ boardList[i].no }"> ${ boardList[i].title } </a>  </th> 
+              <th> ${ boardList[i].writer }  </th> 
+              <th> ${ boardList[i].date }  </th> 
+              <th> ${ boardList[i].view }  </th>  
+            </tr>`
+  }
+  // 3. 출력/대입 
+  boardTbody.innerHTML = html ; 
 }
